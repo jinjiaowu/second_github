cc2ef4052ca0a1ebc92fa89ab1ab1b48d2ac24e1
wujinjiao <1685133357@qq.com>
2026-07-02 09:32:15 +0800

    pycharm提交的内容

---
+++ demo.py
@@ -0,0 +1,16 @@
+# 作者：吴金娇
+# 时间：2026/7/2 08:56
+# 功能：git
+
+print("1")
+
+# pycharm + git 相结合，需要的步骤
+# 1.配置 git，让 pycharm 能够使用 git
+# 2.配置 github 账号（可选）
+# 3.初始化仓库，相当于 git init
+# 4.配置忽略文件 .gitignore
+# 5.添加文件到暂存区，相当于 git add
+# 6.提交文件到本地仓库，相当于 git commit -m "xxx"
+# 7.推送到远程仓库，相当于 git push xxx
+
+
